4b06c2b2672669633e3a26e641e182fee8d5896f
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2025-06-23 19:54:12 +0530

    stateless using JWT token

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,23 @@
-const sessionIdToUserMap= new Map();//basically hashmap
+const jwt = require("jsonwebtoken");
+const secret = "Vrushabh$45@22";
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+function setUser(user){
+    return jwt.sign({
+        _id: user._id,
+        email:user.email,
+    },secret);
+    /*Create a JWT (JSON Web Token) that is digitally signed using a secret key
+This token can be sent to the client for authentication and authorization */
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token) return null;
+    try {
+        return jwt.verify(token,secret);
+    } catch (error) {
+        return null;
+    }
+    
 }
 
 module.exports ={
